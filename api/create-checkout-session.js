87c2b4e9b69c16c9c273afc4f3d3447eedb74e0f
Zinnxs <leonardojoseoliv@gmail.com>
2026-05-21 14:24:21 -0300

    Add files via upload

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -45,7 +45,9 @@ export default async function handler(request, response) {
       mode: 'payment',
       customer_email: customer.email,
       line_items: lineItems,
-      payment_method_types: ['card'],
+      automatic_payment_methods: {
+        enabled: true,
+      },
       billing_address_collection: 'required',
       shipping_address_collection: {
         allowed_countries: ['BR'],
